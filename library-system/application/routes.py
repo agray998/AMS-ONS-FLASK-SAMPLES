b311cfc84d44795053f94385fc3c35021f218335
agray998 <a.gray.4797@gmail.com>
2023-07-28 14:04:44 +0100

    added unit tests, documentation

--- library-system/application/routes.py
+++ library-system/application/routes.py
@@ -3,7 +3,10 @@ from application.book import Book
 from flask import request
 from random import choice
 
-def book_builder(book):
+def book_builder(book) -> dict:
+    '''
+    Creates a serialisable dict from a book object
+    '''
     return {
         "isbn": book.isbn,
         "title": book.title,
@@ -13,7 +16,10 @@ def book_builder(book):
         }
 
 @app.route('/')
-def index():
+def index() -> str:
+    '''
+    Overview of available functionality
+    '''
     return '''
     Library Management System<br>
     Endpoints:<br>
@@ -22,22 +28,37 @@ def index():
     /update -> update a book<br>
     /delete -> delete a book<br>
     /validate -> check the validity of an ISBN
+    /suggested -> get a random recommendation
     '''
 
 @app.route('/search')
-def search():
+def search() -> list:
+    '''
+    Returns a list of books (represented as dicts) written by a given author
+    Author is specified via query param
+    '''
     author = request.args.get("author")
     return list(map(book_builder, Book.search(author)))
 
 @app.route('/new')
-def create():
+def create() -> str:
+    '''
+    Creates a new book object from info supplied via query params
+    Will return custom 403 if the specified ISBN is already taken
+    '''
     if request.args.get("isbn") in map(lambda b: b.isbn, Book.books):
         return f"{request.args.get('isbn')} is already in use", 403
     book = Book(**request.args, fromrepr=True)
     return f"Added new book: {str(book)}"
 
 @app.route('/update/<isbn>')
-def update(isbn):
+def update(isbn: str) -> str:
+    '''
+    Finds a book by ISBN and updates the attributes using info from query params
+    Any attrs not supplied will default to current values (i.e behaves as a PATCH
+    rather than a PUT). Currently does not indicate if ISBN not found - should 
+    update to return custom 404
+    '''
     for book in Book.books:
         if book.isbn.replace("-", "") == isbn.replace("-", ""):
             book.title = request.args.get("title", book.title)
@@ -48,7 +69,11 @@ def update(isbn):
     return f"Updated book with ISBN: {isbn}"
 
 @app.route('/delete/<isbn>')
-def delete(isbn):
+def delete(isbn: str) -> str:
+    '''
+    Finds a book by ISBN and deletes it. Currently does not indicate if 
+    ISBN not found - should update to return custom 404
+    '''
     for i, book in enumerate(Book.books):
         if book.isbn.replace("-", "") == isbn.replace("-", ""):
             del Book.books[i]
@@ -56,14 +81,24 @@ def delete(isbn):
     return f"Deleted book with ISBN: {isbn}"
 
 @app.route('/validate/<isbn>')
-def validate_isbn(isbn):
+def validate_isbn(isbn: str) -> dict:
+    '''
+    Takes an ISBN and returns a dict with:
+    - the given isbn
+    - the validity of the given ISBN as determined by Book.valid_isbn()
+    '''
     return {
         "isbn": isbn,
         "valid": Book.valid_isbn(isbn)
     }
 
 @app.route('/suggested')
-def suggestion():
+def suggestion() -> dict:
+    '''
+    Selects a random book and returns book details as a dict.
+    If a genre is specified as a query param then suggested book 
+    will be of that genre - otherwise any book can be returned
+    '''
     genre = request.args.get("genre", "")
     options = list(filter(lambda b: b.genre == genre, Book.books))
     return book_builder(choice(options if genre else Book.books))
---
+++ library-system/tests/test_app.py
@@ -0,0 +1,96 @@
+from application.routes import create, search, delete, index, validate_isbn, update, suggestion
+from application.book import Book
+from flask import request
+from application import app
+
+# No need to test index since there is no logic, it just returns a literal
+
+def test_create():
+    with app.test_request_context():
+        request.args = {"isbn": "123-4-56-789012-3", "author":"John Smith", "pages":300, "genre": "sci-fi", "title": "sample book"}
+        assert create() == "Added new book: Written by John Smith, sample book is a gripping 300-page sci-fi novel"
+        assert len(Book.books) == 1
+
+def test_create_exists():
+    Book.books = []
+    with app.test_request_context():
+        Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+        request.args = {"isbn": "123-4-56-789012-3", "author":"John Smith", "pages":300, "genre": "sci-fi", "title": "sample book"}
+        assert create() == ("123-4-56-789012-3 is already in use", 403)
+        assert len(Book.books) == 1
+    
+def test_search():
+    Book.books = []
+    Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+    Book(isbn="123-4-56-789012-8", title="Blah2", pages=300, genre="sci-fi", fromrepr=True)
+    Book(isbn="123-4-56-789012-4", title="sample", pages=223, genre="biography", author="James Roberts", fromrepr=True)
+    Book(isbn="123-4-56-789012-6", title="places", pages=100, genre="travel", author="Alice Jones", fromrepr=True)
+    with app.test_request_context():
+        request.args = {"author": "Unknown"}
+        assert search() == [
+            {
+                "isbn": "123-4-56-789012-3",
+                "title": "Blah",
+                "by": "Unknown",
+                "genre": "fantasy",
+                "pages": 200
+            },
+            {
+                "isbn": "123-4-56-789012-8",
+                "title": "Blah2",
+                "by": "Unknown",
+                "genre": "sci-fi",
+                "pages": 300
+            }
+        ]
+
+def test_update():
+    Book.books = []
+    Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+    with app.test_request_context():
+        request.args = {"title": "Updated title"}
+        assert update("123-4-56-789012-3") == "Updated book with ISBN: 123-4-56-789012-3"
+        assert Book.books[0].title == "Updated title"
+
+
+def test_delete():
+    Book.books = []
+    Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+    assert delete("123-4-56-789012-3") == "Deleted book with ISBN: 123-4-56-789012-3"
+    assert len(Book.books) == 0
+    
+
+def test_valid_isbn():
+    assert validate_isbn("123-4-56-789012-3") == {
+        "isbn": "123-4-56-789012-3",
+        "valid": False
+    }
+    assert validate_isbn("123-4-56-789012-8") == {
+        "isbn": "123-4-56-789012-8",
+        "valid": True
+    }
+
+def test_suggestion_genre():
+    Book.books = []
+    Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+    Book(isbn="123-4-56-789012-8", title="Blah2", pages=300, genre="sci-fi", fromrepr=True)
+    Book(isbn="123-4-56-789012-4", title="sample", pages=223, genre="fantasy", author="James Roberts", fromrepr=True)
+    Book(isbn="123-4-56-789012-6", title="places", pages=100, genre="travel", author="Alice Jones", fromrepr=True)
+    with app.test_request_context():
+        request.args = {"genre": "fantasy"}
+        for _ in range(10):
+            b = suggestion()
+            assert b.get("genre") == "fantasy"
+
+def test_suggestion_any():
+    Book.books = []
+    Book(isbn="123-4-56-789012-3", title="Blah", pages=200, genre="fantasy", fromrepr=True)
+    Book(isbn="123-4-56-789012-8", title="Blah2", pages=300, genre="sci-fi", fromrepr=True)
+    Book(isbn="123-4-56-789012-4", title="sample", pages=223, genre="fantasy", author="James Roberts", fromrepr=True)
+    Book(isbn="123-4-56-789012-6", title="places", pages=100, genre="travel", author="Alice Jones", fromrepr=True)
+    with app.test_request_context():
+        request.args = {}
+        for _ in range(10):
+            b = suggestion()
+            assert isinstance(b, dict)
+            assert b.get("by") is not None
